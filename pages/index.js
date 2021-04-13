582b2e504f97c957e5e61cef54107b5df134ca10
JackHowa <jack.howard@washpost.com>
2021-04-12 23:48:31 -0500

    Add back quality

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,7 @@ export default function Home() {
           src={"/page-arizona-elopement-1.jpeg"}
           layout="fill"
           objectFit="cover"
+          quality={100}
           alt="Couple poses with a backdrop of Arizona nature reserve for elopement"
         />
       </main>
